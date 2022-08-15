8dd670892414b0526affd8fb8f52c36c30933033
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2022-08-14 23:24:47 -0300

    Primeiro commit

---
+++ Aula02/src/ProfessorSamples.java
@@ -0,0 +1,22 @@
+import java.security.KeyStore;
+
+public class ProfessorSamples {
+
+    public static void main(String[] args) {
+
+        int numero = 30;
+        String nome = "Messias Rafael";
+        double pi = 3.1414;
+        boolean status = true;
+
+        System.out.println(status);
+
+        float result, teste;
+        teste = 56.89f;
+        result = (float) (numero + (numero * 0.1));
+        if (result > 35) {
+        System.out.println(result);
+        System.out.println(teste); }
+
+    }
+}
